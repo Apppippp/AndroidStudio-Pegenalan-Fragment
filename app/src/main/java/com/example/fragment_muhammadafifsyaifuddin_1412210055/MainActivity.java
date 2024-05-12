105e464ac37696f93bf61628f0299ce19d19e442
LENOVO <afifsyaifn27@gmail.com>
2024-05-12 12:11:59 +0700

    android-pengenalan-fragment

---
+++ app/src/main/java/com/example/fragment_muhammadafifsyaifuddin_1412210055/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fragment_muhammadafifsyaifuddin_1412210055;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
